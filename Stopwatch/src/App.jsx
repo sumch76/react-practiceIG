7bb201ecdd5b52b05b55d0ecf45c90b54f27b555
sumch76 <sumitchaubey76@gmail.com>
2024-09-19 19:32:17 +0530

    timeCounter

---
+++ Stopwatch/src/App.jsx
@@ -0,0 +1,13 @@
+import './Component/CounterTimer.css'
+import CounterTimer from './Component/CounterTimer'
+
+function App() {
+  
+  return(
+  <div className='App'>
+    <CounterTimer/>
+  </div>
+  )
+}
+
+export default App
---
+++ Stopwatch/src/Component/CounterTimer.jsx
@@ -0,0 +1,78 @@
+import React, { useEffect } from 'react'
+import { useState,useRef } from 'react';
+import "./CounterTimer.css"
+
+function CounterTimer() {
+  const[time,setTime]=useState(0);
+  const[isActive,setActive]=useState(false);
+  const[isPause,setPause]=useState(true);
+  const intervalRef=useRef("null");
+
+  const handleInput=(e)=>{
+    setTime(parseInt(e.target.value*60))
+  }
+  //changing minute into second when user input the value in  minute
+
+function formatTime(){
+  const min=String(Math.floor(time/60)).padStart(2,'0');
+  const sec=String(time%60).padStart(2,'0');
+  return `${min}:${sec}`;
+}
+
+function handleStart()
+{
+  setActive(true);
+  setPause(false);
+
+}
+useEffect(()=>{
+  if(isActive && !isPause && time>0)
+  {
+    intervalRef.current=setInterval(()=>
+    {
+      setTime((prev)=>prev-1)
+    },1000)
+  }
+  else if(time===0)
+  {
+    clearInterval(intervalRef.current);
+    setActive(false);
+    alert("time is  up");
+  }
+
+  return()=>clearInterval(intervalRef.current);
+},[isActive,isPause,time]);
+
+function handlePause(){
+setPause(!isPause);
+
+}
+
+const handleReset=()=>
+{
+  clearInterval(intervalRef.current);
+  setActive(false);
+  setPause(false);
+  setTime(0);
+}
+
+  return (
+    <div className='countdown-timer'>
+      <h1>Countdown Timer</h1>
+      <div className="timer-display">
+        <input type='number' placeholder="enter time in minutes"
+        onChange={handleInput}/>
+        <div>{formatTime()}</div>
+      
+      </div>
+      <div className="timer-controls">
+        <button onClick={handleStart} disabled={isActive && !isPause}>Start</button>
+        <button onClick={handlePause} disabled={!isActive}>{isPause?'Resume':'Pause'}</button>
+        <button onClick={handleReset}>Reset</button>
+      </div>
+    
+    </div>
+  )
+}
+
+export default CounterTimer
