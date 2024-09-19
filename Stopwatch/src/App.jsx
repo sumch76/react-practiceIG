3abe57a4e705dc992daa18151e68d69ec9ecaa99
sumch76 <sumitchaubey76@gmail.com>
2024-09-19 21:13:08 +0530

    timer example of useRef

--- Stopwatch/src/App.jsx
+++ Stopwatch/src/App.jsx
@@ -1,15 +1,17 @@
 import './Component/CounterTimer.css'
 import CounterTimer from './Component/CounterTimer'
 import UseReference from './UseRefExample/UseReference';
+import UseReference2 from './UseRefExample/UseReference2';
 
 function App() {
   
   return(
   <div className='App'>
     <CounterTimer/>
-    {/* <UseReference/> */}
+    {/* <UseReference/>
+    <UseReference2/> */}
   </div>
   )
 }
 
-export default App
+export default App;
---
+++ Stopwatch/src/UseRefExample/UseReference2.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { useRef ,useState} from 'react';
+
+const UseReference2 = () => {
+    const[second,setSecond]=useState(0);//state for timer
+    const timeRef=useRef(null);
+
+    const startTimer=()=>
+    {
+        if(timeRef.current!=null)
+            return;
+        //if timr is already running do nothing
+        timeRef.current=setInterval(()=>
+        {
+            setSecond(prev=>prev+1);  //increase  timer in each second
+        },1000);
+    };
+
+    const stopTimer=()=>{
+        clearInterval(timeRef.current); //stop timer
+        timeRef.current=null; //reset ref
+    }
+
+
+    const resetTimer=()=>{
+        clearInterval(timeRef.current);
+        timeRef.current=null;
+        setSecond(0);  //reset timer
+    };
+  return (
+    <div>
+        <h1>Timer:{second}</h1>
+        <button onClick={startTimer}>start</button>
+        <button onClick={stopTimer}>Stop</button>
+        <button onClick={resetTimer}>Reset</button>
+      
+    </div>
+  )
+}
+
+export default UseReference2;
