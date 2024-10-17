e733202ad63f23df5d10a5a593e970cf6ce09102
sumch76 <sumitchaubey76@gmail.com>
2024-10-17 23:12:22 +0530

    wordle

---
+++ useTheme/App.js
@@ -0,0 +1,31 @@
+import { useState } from "react"
+
+export const useTheme = () => {
+	// Write your hook logic here
+  const [theme,setTheme]=useState('light');
+
+ const toggleTheme=()=>
+ {
+	setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+ }
+	return {
+		theme,
+        toggleTheme,
+	}
+}
+const App = () => {
+	const { theme, toggleTheme } = useTheme()
+	return (
+		<div
+			style={{
+				height: '100vh',
+				transition: '0.3s ease-in',
+				backgroundColor: theme === 'light' ? 'white' : 'black',
+			}}
+		>
+			<button onClick={toggleTheme}>Toggle Theme</button>
+		</div>
+	)
+}
+
+export default App
