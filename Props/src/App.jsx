3b805c5e75c936645046d689e43342b4cb50112c
sumch76 <sumitchaubey76@gmail.com>
2024-10-19 22:16:20 +0530

    usercontext project simple using theme toggle

--- Props/src/App.jsx
+++ Props/src/App.jsx
@@ -3,14 +3,26 @@ import './App.css'
 import GrandParent from "./components/GrandParent";
 import { createContext, useState } from "react";
 const UserContext=createContext();
+const ThemeContext=createContext();
 function App() {
  
   const[user,setUser]=useState({name:"Sumit"});
+  const [theme,setTheme]=useState('light')
 
   return (
     <>
     <UserContext.Provider value={user}>
-      <GrandParent />
+      <ThemeContext.Provider value={{theme,setTheme}}>
+        <div id='container' style={{backgroundColor:theme==='light'?
+          "beige":"black"
+        }}>
+    <GrandParent />
+        </div>
+
+        </ThemeContext.Provider>
+
+  
+    
     </UserContext.Provider>
  
     {/* <ParentComponent/> */}
@@ -20,3 +32,4 @@ function App() {
 
 export {UserContext}
 export default App;
+export {ThemeContext}
--- Props/src/components/Child.jsx
+++ Props/src/components/Child.jsx
@@ -1,13 +1,22 @@
 import { useContext } from 'react';
 import React from  'react'
-import { UserContext } from '../App';
+import { ThemeContext, UserContext } from '../App';
 
 const Child = () => {
     const user=useContext(UserContext);
+    const {theme,setTheme}=useContext(ThemeContext);
+    const handleTheme=()=>{
+      if(theme==='light')
+        setTheme('dark')
+      else
+      setTheme('light')
+    }
   return (
     <div>
+       
+        <button onClick={handleTheme}>Toggle Theme</button>
         data:{user.name}
-      
+       
     </div>
   )
 }
