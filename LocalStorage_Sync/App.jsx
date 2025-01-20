adac480060d4d66da1f4084b2291eeb212cdb534
sumch76 <sumitchaubey76@gmail.com>
2025-01-20 19:56:56 +0530

    local storage localStorage with  add. feature

--- LocalStorage_Sync/App.jsx
+++ LocalStorage_Sync/App.jsx
@@ -19,4 +19,47 @@ const App = () => {
     </>
   );
 };
+
 export default App;
+
+//additional feature  i have added
+
+import { useEffect, useState } from 'react';
+
+const App2 = () => {
+  const number = Number(localStorage.getItem('count'));
+
+  const [count, setCount] = useState(number);
+  useEffect(() => {
+    localStorage.setItem('count', count);
+  }, [count]);
+
+  const prevCount = () => {
+ 
+    setCount((count) => count - 1);
+    if (count < 1) {
+      alert('no. cannot be negative');
+      setCount(0);
+    }
+  };
+
+  const handleReset = () => {
+    setCount(0);
+  };
+
+  return (
+    <>
+      <h1>counter app</h1>
+      <div>
+        <p>Count is {count}</p>
+        <button onClick={() => setCount((count) => count + 1)}>
+          add count
+        </button>
+        <button onClick={prevCount}>sub count</button>
+        <button onClick={handleReset}>Reset count</button>
+      </div>
+    </>
+  );
+};
+
+
